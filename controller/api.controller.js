fcd7745861b1b08ba34942de95202b2ffeeb5504
milenaff <mih.ferreiradepaula@gmail.com>
2021-11-14 12:18:34 -0300

    Método Show Images adicionado (versão 1.0)

--- controller/api.controller.js
+++ controller/api.controller.js
@@ -2,11 +2,28 @@ var express = require('express');
 var router = express.Router();
 var apiservice = require('../services/api.service');
 router.get('/list', getAll)
+router.get('/listimg', getAllImages)
 
 module.exports = router;
 
 function getAll(req, res) {
     apiservice.list().then(function (d) {
+        debugger;
+        if (d) {
+            res.send(d);
+        } else {
+            res.sendStatus(404);
+        }
+
+    }).catch(function (err) {
+        res.status(400).send(err);
+    });
+
+}
+
+//Método show images
+function getAllImages(req, res) {
+    apiservice.listimg().then(function (d) {
         debugger;
         if (d) {
             res.send(d);
--- services/api.service.js
+++ services/api.service.js
@@ -2,6 +2,7 @@ var Q = require('q');
 var service = {};
 
 service.list = getAllContainers;
+service.listimg= getAllImages;
 
 module.exports = service;
 
@@ -39,5 +40,35 @@ function getAllContainers() {
     return deferred.promise;
 }
 
+function getAllImages() {
+    var deferred = Q.defer();
+
+    const options = {        
+        hostname: 'localhost',
+        port: 2375,        
+        path: '/images/json?all=true',
+        headers: {
+        },
+        method: 'GET',
+    }
+
+    const req = https.request(options, res => {
+        console.log(`statusCode: ${res.statusCode}`)
+
+        res.on('data', function (chunk) {
+            debugger;
+             //process.stdout.push(d);
+             var resultado  = chunk;             
+             deferred.resolve(JSON.parse(resultado));
+        })
+    })
+
+    req.on('error', error => {
+        console.error(error)
+    })
+
+    req.end();
+    return deferred.promise;
+}
 //alteração service 
 
